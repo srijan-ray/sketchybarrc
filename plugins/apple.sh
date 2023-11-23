997169ded1fc5e1dceb359bddd7a44e1058f1264
Srijan Ray <srijanray12@gmail.com>
2023-11-22 22:05:15 -0500

    Made major changes to UI including making bar transparent and groups with nice dark bluish background

--- plugins/apple.sh
+++ plugins/apple.sh
@@ -5,12 +5,17 @@ POPUP_CLICK_SCRIPT='sketchybar --set $NAME popup.drawing=toggle'
 
 apple_logo=(
   icon=
-  icon.font="Hack Nerd Font:Bold:23.0" 
+  icon.font="Hack Nerd Font:Bold:23.0"
   icon.color=0xfff5a97f
+  icon.padding_right=10
+  icon.padding_left=10
   padding_right=15
   label.drawing=off
   click_script="$POPUP_CLICK_SCRIPT"
   popup.height=35
+  background.height=28
+  background.color=0xff2b4059
+  background.corner_radius=25
 )
 
 apple_prefs=(
