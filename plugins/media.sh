733e49255b4be92653152a8428c9a24df2ef7b1a
Srijan Ray <srijanray12@gmail.com>
2023-12-14 23:10:24 -0500

    Changed media plugin to show only 15 characters instead of 18 characters

--- plugins/media.sh
+++ plugins/media.sh
@@ -2,7 +2,7 @@
 STATE="$(echo "$INFO" | jq -r '.state')"
 
 if [ "$STATE" = "playing" ]; then
-  MEDIA="$(echo "$INFO" | jq -r '.title' | sed 's/\(.\{18\}\).*/\1/')"
+  MEDIA="$(echo "$INFO" | jq -r '.title' | sed 's/\(.\{15\}\).*/\1/')"
   sketchybar --set $NAME label="$MEDIA" drawing=on
 else
   sketchybar --set $NAME drawing=off
