4fa31b7008118acc3fda8b57854c98170160281c
Srijan Ray <srijanray12@gmail.com>
2024-07-08 00:14:52 -0400

    feat(cpu): Changed CPU widget to have a graph instead of just percent

--- plugins/cpu.sh
+++ plugins/cpu.sh
@@ -1,5 +1,26 @@
 #!/usr/bin/env bash
 #
 
+source "$HOME/.config/sketchybar/colors.sh"
 
-sketchybar --set $NAME label="$(top -l  2 | grep -E "^CPU" | tail -1 | awk '{ print $3 + $5"%" }' | cut -d "." -f1)"% 
+CORE_COUNT=$(sysctl -n machdep.cpu.thread_count)
+CPU_INFO=$(ps -eo pcpu,user)
+CPU_SYS=$(echo "$CPU_INFO" | grep -v $(whoami) | sed "s/[^ 0-9\.]//g" | awk "{sum+=\$1} END {print sum/(100.0 * $CORE_COUNT)}")
+CPU_USER=$(echo "$CPU_INFO" | grep $(whoami) | sed "s/[^ 0-9\.]//g" | awk "{sum+=\$1} END {print sum/(100.0 * $CORE_COUNT)}")
+
+
+CPU_PERCENT="$(echo "$CPU_SYS $CPU_USER" | awk '{printf "%.0f\n", ($1 + $2)*100}')"
+
+COLOR=$ACTUAL_WHITE
+case "$CPU_PERCENT" in
+  [1-2][0-9]) COLOR=$ACTUAL_WHITE
+  ;;
+  [3-6][0-9]) COLOR=$ORANGE
+  ;;
+  [7-9][0-9]|100) COLOR=$RED
+  ;;
+esac
+
+sketchybar --set  cpu.percent label=$CPU_PERCENT% \
+                              label.color=$COLOR  \
+           --push cpu.user    $CPU_USER
