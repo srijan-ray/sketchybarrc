38de62dffe27c9ce44dab9c4ddeb21f9c8878e94
Srijan Ray <srijanray12@gmail.com>
2024-07-08 17:55:57 -0400

    feat(RAM): Added same graph widget as CPU for RAM

--- plugins/memory.sh
+++ plugins/memory.sh
@@ -1,5 +1,21 @@
 #!/usr/bin/env bash
 #
 
+source "$HOME/.config/sketchybar/colors.sh"
 
-sketchybar --set $NAME label="$(memory_pressure | grep "System-wide memory free percentage:" | awk '{ printf("%02.0f\n", 100-$5"%") }')"% 
+MEMORY_USAGE=$(memory_pressure | grep "System-wide memory free percentage:" | awk '{ printf("%02.0f\n", 100-$5"%") }')
+MEMORY_GRAPH=$(memory_pressure | grep "System-wide memory free percentage:" | awk '{ printf("%0.02f\n", (100-$5)/100"%") }')
+
+COLOR=$ACTUAL_WHITE
+case "$MEMORY_USAGE" in
+  [1-2][0-9]) COLOR=$ACTUAL_WHITE
+  ;;
+  [3-6][0-9]) COLOR=$ORANGE
+  ;;
+  [7-9][0-9]|100) COLOR=$RED
+  ;;
+esac
+
+sketchybar --set ram.percent label="RAM: ${MEMORY_USAGE}%" \
+                              label.color=$COLOR  \
+           --push ram.user    ${MEMORY_GRAPH}
